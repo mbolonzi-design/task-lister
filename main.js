02959ed74b0af2435addab092f9b3bebb8884cf4
mbolonzi-design <mbolonzi.design@gmail.com>
2023-06-18 12:38:53 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        buildToDo(e.target.new_todo.value)
+        form.reset();
+    })
+})
+
+function buildToDo(todo){
+    let p = document.createElement("p");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDelete)
+    btn.textContent = "x"
+    p.textContent = `${todo} `
+    p.style.backgroundColor = "gold"
+    p.appendChild(btn);
+    document.querySelector("#todos_list").appendChild(p);
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
